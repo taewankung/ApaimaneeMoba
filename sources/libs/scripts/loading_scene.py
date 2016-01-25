1a24ce2158ab30c555b10bb09159b2252d94fa82
Phutthewan Yangyuenyong <taewankung@gmail.com>
2016-01-25 13:17:18 +0700

    loading_scene make function

--- sources/libs/scripts/loading_scene.py
+++ sources/libs/scripts/loading_scene.py
@@ -1,8 +1,8 @@
 from bge import logic
 import time
 
-
-cont = logic.getCurrentController()
-scene_act = cont.actuators["Scene"]
-cont.activate(scene_act)
-print("ss")
+def loading_scene():
+    cont = logic.getCurrentController()
+    scene_act = cont.actuators["Scene"]
+    cont.activate(scene_act)
+    print("ss")
