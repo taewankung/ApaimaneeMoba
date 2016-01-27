a7c0a2e1c8479216f79b00d815bfaf438fc170d5
Phutthewan Yangyuenyong <taewankung@gmail.com>
2016-01-27 16:12:31 +0700

    change gameunit

--- sources/libs/apaimanee/characters/GameUnit.py
+++ sources/libs/apaimanee/characters/GameUnit.py
@@ -2,42 +2,24 @@ from bge import logic
 import uuid
 
 class GameUnit:
-    def __init__(self,
-                 #name, 
-                 controller,
-		         #max_hp, # maximum helth for game unit
-                 #damaged=0,
-                 #speed=0,
-                 #attack_speed=0,
-                 #armor = 1,
-                 #act_message = 'None',
-		         #sensor_message = 'None',
-                 unit='',
-                 enemy_list = []
+    def __init__(self
                  ):
+        self.id = str(uuid.uuid4())
+        self.controller = logic.getCurrentController()
+        self.owner = controller.owner
 
-        if 'id' in controller.owner:
-            if len(controller.owner['id']) == 0:
-                self.id = str(uuid.uuid4())
-                controller.owner['id'] = self.id
-            else:
-                self.id = controller.owner['id']
-
-        self.cont = controller
-        self.unit=unit
-        self.enemy_list = enemy_list
        
     def reduce_hp(self, damaged):
-        self.unit["hp"] = damaged*((100-self.unit["armor"])/100)
+        self.owner["hp"] = damaged*((100-self.owner["armor"])/100)
 
     def regend_hp(self,hp):
-        self.unit["hp"] += hp
+        self.owner["hp"] += hp
 
     def die(self):
-        self.unit["alive"] = False
+        self.owner["alive"] = False
 
     def get_alive(self):
-        self.unit["alive"] = True
+        self.owner["alive"] = True
     #///////////////////// Interface ////////////////////////
     def move_unit(self):
         pass
@@ -46,13 +28,13 @@ class GameUnit:
         pass
     
     def get_owner(self):
-        return self.unit
+        return self.owner
 
     def show_status(self):
-        print("Name: ", self.unit["name"])
-        print("hp: ", self.unit["hp"])
-        print("id: ",id(self.unit))
-        if(self.unit["alive"] == False ):
+        print("Name: ", self.owner["name"])
+        print("hp: ", self.owner["hp"])
+        print("id: ",id(self.owner))
+        if(self.owner["alive"] == False ):
            print("alive status: Die")
         else:
            print("alive status: Alive")
@@ -60,15 +42,15 @@ class GameUnit:
     def damaged(self):
         scene = logic.getCurrentScene()
         obj = None
-        if self.cont.sensors["Message"].positive and self.cont.sensors["id_message"].positive:
-            enemy = self.cont.sensors["Message"].bodies[0]
+        if self.controller.sensors["Message"].positive and self.controller.sensors["id_message"].positive:
+            enemy = self.controller.sensors["Message"].bodies[0]
             #print(enemy)#enemy attack
-            id_mine = self.cont.sensors["id_message"].bodies[0]
-            if self.cont.sensors["id_message"].bodies[0] == self.unit["id"]:
+            id_mine = self.controller.sensors["id_message"].bodies[0]
+            if self.controller.sensors["id_message"].bodies[0] == self.owner["id"]:
                 if enemy in scene.objects:
                     obj = scene.objects[enemy]#
-                if self.unit["id"] == id_mine:
+                if self.owner["id"] == id_mine:
                     #print("yes")
-                    if "hp" in self.unit and "hp" in obj:
-                        if self.unit["hp"] > 0:
-                            self.unit["hp"] = self.unit["hp"]-float(obj["dmg"])
+                    if "hp" in self.owner and "hp" in obj:
+                        if self.owner["hp"] > 0:
+                            self.owner["hp"] = self.owner["hp"]-float(obj["dmg"])
--- sources/libs/apaimanee/characters/minion/minion.py
+++ sources/libs/apaimanee/characters/minion/minion.py
@@ -2,42 +2,38 @@ from bge import logic
 from libs.apaimanee.characters.GameUnit import GameUnit
 import math
 
+import uuid
+
+
 class Minion(GameUnit):
     def __init__(self, 
-                 #checkpoint,
                  controller,
-                 animation,
-                 unit,
                  skeleton_name,
-                 creep_action,
-                 enemy_list=[]):
-        super().__init__(
-                        controller,
-                        unit,
-                        enemy_list,
-                        )
+                 creep_action,):
+        super().__init__(controller)
+
         #self.checkpoint = checkpoint
-        self.track = self.cont.actuators["track"]
-        self.animation = self.cont.actuators["animation"]
-        self.near = self.cont.sensors["Near"]
-        self.col_enemy = self.cont.sensors["col_enemy"]
-        self.move = self.cont.actuators["Move"]
-        self.attack_act = self.cont.actuators["attack_mes"]
+        self.track = self.controller.actuators["track"]
+        self.animation = self.controller.actuators["animation"]
+        self.near = self.controller.sensors["Near"]
+        self.col_enemy = self.controller.sensors["col_enemy"]
+        self.move = self.controller.actuators["Move"]
+        self.attack_act = self.controller.actuators["attack_mes"]
         self.scene = logic.getCurrentScene()        
         self.skeleton_name = skeleton_name
         self.creep_action = creep_action
     
     def set_first_path(self,first_path):
-        if self.unit["init"] == 0 :
-            self.unit["mem_path"] = first_path 
-            self.unit["init"]=1
+        if self.owner["init"] == 0 :
+            self.owner["mem_path"] = first_path 
+            self.owner["init"]=1
 
     def move_unit(self):
-        self.track.object = self.scene.objects[self.unit["mem_path"]]
-        self.cont.activate(self.track)
-        if self.cont.sensors["col_path"].positive :
-            self.track.object = self.cont.sensors["col_path"].hitObject["path"]
-            self.unit["mem_path"]= self.cont.sensors["col_path"].hitObject["path"]
+        self.track.object = self.scene.objects[self.owner["mem_path"]]
+        self.controller.activate(self.track)
+        if self.controller.sensors["col_path"].positive :
+            self.track.object = self.controller.sensors["col_path"].hitObject["path"]
+            self.owner["mem_path"]= self.controller.sensors["col_path"].hitObject["path"]
         
         if self.near.positive:
             hit_objs = self.near.hitObjectList
@@ -46,28 +42,28 @@ class Minion(GameUnit):
             count = 0
             for item in hit_objs:
                 if "team" in item:
-                    if item["team"] != self.unit["team"]:
-                        if item.getDistanceTo(self.unit) < dist or dist ==0:
-                            dist = item.getDistanceTo(self.unit)
+                    if item["team"] != self.owner["team"]:
+                        if item.getDistanceTo(self.owner) < dist or dist ==0:
+                            dist = item.getDistanceTo(self.owner)
                             obj = item
                             self.track.object = obj
-                            self.cont.activate(self.move)
-                            self.cont.activate(self.track)
-                            self.unit["states"]="move"
+                            self.controller.activate(self.move)
+                            self.controller.activate(self.track)
+                            self.owner["states"]="move"
 
                             if self.col_enemy.positive:
-                                self.cont.deactivate(self.move)
+                                self.controller.deactivate(self.move)
                                 self.attack(obj)
-                                self.unit["states"]="attack"
-                    if item["team"] == self.unit["team"]:
+                                self.owner["states"]="attack"
+                    if item["team"] == self.owner["team"]:
                         count = count+1
                     if count == len(hit_objs):
-                        self.unit["states"]="move"
+                        self.owner["states"]="move"
         
-        if self.unit["states"]=="move":
-            self.cont.activate(self.track)
-            self.cont.activate(self.move)
-            for bone in self.unit.children:    
+        if self.owner["states"]=="move":
+            self.controller.activate(self.track)
+            self.controller.activate(self.move)
+            for bone in self.owner.children:    
                 if bone.name == self.skeleton_name:
                     bone.playAction(self.creep_action,
                                      10,68,
@@ -75,7 +71,7 @@ class Minion(GameUnit):
                                      speed=1)
     
     def attack(self,enemy):
-        for bone in self.unit.children:
+        for bone in self.owner.children:
             if bone.name == self.skeleton_name:
                 bone.playAction(self.creep_action,
                                           90,117.0000000000000000000000000000,
@@ -83,12 +79,12 @@ class Minion(GameUnit):
                                           speed=1)
                 if math.fabs(bone.getActionFrame()-117)< 99e-2 :
                     bone.stopAction()
-                    self.unit.sendMessage("attack",self.unit.name,str(enemy))
-                    self.unit.sendMessage("attack_unitID",enemy["id"],str(enemy))
+                    self.owner.sendMessage("attack",self.owner.name,str(enemy))
+                    self.owner.sendMessage("attack_unitID",enemy["id"],str(enemy))
      
     def die_and_gold(self):
-        if self.cont.sensors["Message"].positive:
-            enemy = self.cont.sensors["Message"].bodies[0]
-            self.unit.sendMessage("reward","90",enemy)
-            self.unit.endObject()
+        if self.controller.sensors["Message"].positive:
+            enemy = self.controller.sensors["Message"].bodies[0]
+            self.owner.sendMessage("reward","90",enemy)
+            self.owner.endObject()
             pass
