dd10e8efa621e0e68001b2f311c39070313acd96
Thanathip Limna <boatkrap@gmail.com>
2016-01-30 20:38:40 +0700

    remove binary

--- sources/libs/scripts/loading_scene.py
+++ sources/libs/scripts/loading_scene.py
@@ -1,8 +1,10 @@
 from bge import logic
 import time
 
+import sys
+
 def loading_scene():
     cont = logic.getCurrentController()
     scene_act = cont.actuators["Scene"]
     cont.activate(scene_act)
-    print("ss")
+    print("arg:", sys.argv)
