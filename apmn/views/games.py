39be871f1d3c08cbd9736172297e346358b535c7
Aran Khunaree <alunnice2537@gmail.com>
2016-02-06 23:07:59 +0700

    fixed create_room in games.py

--- apmn/views/games.py
+++ apmn/views/games.py
@@ -1,5 +1,6 @@
 from apmn.forms import games as games_form
-
+import logging
+logger = logging.getLogger("apmn")
 def create_room(request):
     if request.session.get('room_name',None):
         return request.redirect_url('/games/create_room')
@@ -16,6 +17,20 @@ def create_room(request):
                     form = form,
                     message = 'Please Room Name.'
                     )
+
+    try:
+        data = request.apmn_client.room.create_room(room_name)
+        if not data['responses']['room_id']:
+            raise 'Not Found Room ID'
+
+    except Exception as e:
+        logger.exception(e)
+        logger.error("error: ", data)
+        return dict(
+                message=data.get("error", "Can't Create Room"),
+                form = form
+                )
+
 
     return request.redirect_url('/games/room')
 
